c8d1590cfbb4273cfcb46401c3aec4db8e40e881
ROCKTAKEY <rocktakey@gmail.com>
2025-05-06 15:58:06 +0900

    Indent

--- up.sh
+++ up.sh
@@ -5,11 +5,11 @@ VERSION=0.34
 docker pull docker.all-hands.dev/all-hands-ai/runtime:${VERSION}-nikolaik
 
 docker run --rm --pull=always \
-    -e SANDBOX_RUNTIME_CONTAINER_IMAGE=docker.all-hands.dev/all-hands-ai/runtime:${VERSION}-nikolaik \
-    -e LOG_ALL_EVENTS=true \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v ~/.openhands-state:/.openhands-state \
-    -p 3000:3000 \
-    --add-host host.docker.internal:host-gateway \
-    --name openhands-app \
-    docker.all-hands.dev/all-hands-ai/openhands:${VERSION}
+	-e SANDBOX_RUNTIME_CONTAINER_IMAGE=docker.all-hands.dev/all-hands-ai/runtime:${VERSION}-nikolaik \
+	-e LOG_ALL_EVENTS=true \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v ~/.openhands-state:/.openhands-state \
+	-p 3000:3000 \
+	--add-host host.docker.internal:host-gateway \
+	--name openhands-app \
+	docker.all-hands.dev/all-hands-ai/openhands:${VERSION}
